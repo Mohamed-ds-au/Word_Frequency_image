ad2622c73b65258baeb1227334dd452910f9d069
ZIZO-ELKHAT_TEB <130157238+ZizoElkhateeb@users.noreply.github.com>
2024-04-21 01:36:28 +0200

    Upload files of program

---
+++ image-word/Assignment.py
@@ -0,0 +1,29 @@
+import nltk
+from nltk.corpus import stopwords
+from collections import Counter
+
+#nltk.download('stopwords')
+#nltk.download('punkt')
+
+#read txt file 
+with open("paragraphs.txt", 'r') as par:
+    text = par.read()
+
+stop_words = set(stopwords.words('english'))
+
+#print(stop_words)
+
+from nltk.tokenize import word_tokenize ,sent_tokenize
+tokenize_words = word_tokenize(text)
+
+tokenize_words_without_stop_words =[]
+for word in tokenize_words:
+    if word.lower() not in stop_words:
+        tokenize_words_without_stop_words.append(word)
+
+#print(tokenize_words_without_stop_words)
+
+word_freq = Counter(tokenize_words_without_stop_words)
+
+for word , freq in word_freq.most_common():
+    print("f{} : {}".format(word , freq))
